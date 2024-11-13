9ced4a44397f4a0e16271b0c9ed4363c24d46e89
S0okJu <damekyung98@gmail.com>
2024-11-13 22:36:55 +0900

    [S0okJu] Session02

--- 백준/1번문제/S0okJu/Q1620.py
+++ 백준/1번문제/S0okJu/Q1620.py
@@ -28,27 +28,28 @@ def timeout():
 
 1. 아이디어 
 - 키를 통해 값을 알아야 하고 값을 통해 키를 알아야 한다.
-- 딕셔너리 하나로는 구현하기 어려울 것이라고 생각했다.
+- 중복이 없어야 한다.
+- 딕셔너리 하나로는 구현하기 어려울 것이라고 생각했다. 
 
 2. 시간복잡도 :
-O( )
-3. 자료구조 :
+O(n)
+3. 자료구조 : 구현
 
 """
 
-
 def solution():
     n, m = map(int, input().split())
-    pokemons = {}  
+    poks = {}  
     
     for i in range(1, n + 1):
         pok = input().strip()
-        pokemons[str(i)] = pok  
-        pokemons[pok] = str(i)  
+        poks[str(i)] = pok  
+        poks[pok] = str(i)  
 
     for _ in range(m):
         quiz = input().strip()
         
-        print(pokemons[quiz])
+        print(poks[quiz])
+        
 if __name__ == '__main__':
     solution()
